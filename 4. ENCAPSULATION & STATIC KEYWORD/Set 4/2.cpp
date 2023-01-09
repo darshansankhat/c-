18f1c93552f4aa568307f5f1642bad02a7301f43
darshansankhat <119835050+darshansankhat@users.noreply.github.com>
2023-01-09 10:17:24 +0530

    Update 2.cpp

--- 4. ENCAPSULATION & STATIC KEYWORD/Set 4/2.cpp
+++ 4. ENCAPSULATION & STATIC KEYWORD/Set 4/2.cpp
@@ -1,3 +1,4 @@
+ WAP to create a class which Read and print Class, Student details using Two Classes. (Make two classes, create one classe's obj in another class.)
 #include<iostream>
 #include<string.h>
 using namespace std;
